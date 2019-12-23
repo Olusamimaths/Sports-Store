c34df686db6889bd48982d53aa6c83d41fe3ebbd
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-12-23 16:19:22 +0100

    Implemented graphql to react connector

--- src/admin/components/ordersConnector.js
+++ src/admin/components/ordersConnector.js
@@ -1,2 +1,23 @@
 import { graphql } from "react-apollo";
 import { ordersSummaryQuery } from '../client/clientQueries';
+import { OrdersTable } from './OrdersTable';
+
+const vars = {
+    onlyShipped: false, page: 1, pageSize: 10, sort: 'id'
+}
+
+export const OrdersConnector = graphql(ordersSummaryQuery, 
+    {
+        options: (props)=> ({variables: vars}),
+        props: ({data: {loading, orders, refetch}}) => ({
+            totalSize: loading ? 0 : orders.totalSize,
+            orders: loading ? [] :orders.orders,
+            currentPage: vars.page,
+            pageCount: loading ? 0 : Math.ceil(orders.totalSize/vars.pageSize),
+            navigateToPage: (page) => {vars.page= Number(page); refetch(vars)},
+            pageSize: vars.pageSize,
+            setPageSize: (size) => {vars.pageSize = Number(size); refetch(vars)},
+            sortKey: vars.sort,
+            setSortKey: (key) => {vars.sort = key; refetch(vars)}
+        }) 
+    })(OrdersTable)
