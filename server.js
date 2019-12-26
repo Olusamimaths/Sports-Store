e2570a2112c76b5b8e396587e59c0a1e40b1409d
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-12-26 17:10:34 +0100

    Prepared app for production

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const graphqlHTTP = require('express-graphql')
 const queryResolvers = require('./serverQueriesResolver');
 const mutationResolvers = require('./serverMutationsResolver');
 let auth = require('./authMiddleware');
+const history = require('connect-history-api-fallback');
 
 const fileName = process.argv[2] || './data.js';
 const port = process.argv[3] || 3500;
@@ -36,6 +37,8 @@ createServer();
 
 const app = express();
 
+app.use(history());
+app.use('/', express.static('./build'));
 app.use(cors());
 app.use(jsonServer.bodyParser);
 app.use(auth);
